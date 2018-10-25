418cb50bd4116a8f03eef6e974f0a7c04a090754
AuroreD <aderrien@hotmail.fr>
2018-10-25 15:50:51 +0200

    commit context

---
+++ src/main/java/ProjetAeroport/util/Context.java
@@ -0,0 +1,33 @@
+package ProjetAeroport.util;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Context {
+
+	private EntityManagerFactory emf = null;
+	
+	private static Context ctx = null;
+	
+	private Context() {
+		emf = Persistence.createEntityManagerFactory("ProjetAeroport");
+	}
+	
+	public static Context getInstance() {
+		if(ctx == null) {
+			ctx = new Context();
+		}
+		return ctx;
+	}
+	
+	public EntityManagerFactory getEntityManagerFactory() {
+		return emf;
+	}
+	
+	public static void destroy() {
+		if(ctx != null) {
+			ctx.emf.close();
+			ctx = null;
+		}
+	}
+}
