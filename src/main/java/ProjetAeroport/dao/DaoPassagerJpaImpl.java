031f7057bf54bcaad8e2a8645ecbaf8a75c2f88c
vallansell <vallan95@hotmail.fr>
2018-10-26 11:17:18 +0200

    git commit Dao

---
+++ src/main/java/ProjetAeroport/dao/DaoPassagerJpaImpl.java
@@ -0,0 +1,123 @@
+package ProjetAeroport.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+
+import ProjetAeroport.model.Passager;
+
+import ProjetAeroport.util.*;
+
+class DaoPassagerJpaImpl implements DaoPassager {
+
+	@Override
+	public void create(Passager obj) {
+		EntityManager em = Context.getInstance().getEntityManagerFactory().createEntityManager();
+		EntityTransaction tx = null;
+		try {
+			tx = em.getTransaction();
+			tx.begin();
+			em.persist(obj);
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+		} finally {
+			if (em != null && em.isOpen()) {
+				em.close();
+			}
+		}
+
+	}
+
+	@Override
+	public Passager findByKey(Long key) {
+		EntityManager em = Context.getInstance().getEntityManagerFactory().createEntityManager();
+		Passager recherche = null;
+		recherche = em.find(Passager.class, key);
+		em.close();
+		return recherche;
+	}
+
+	@Override
+	public Passager update(Passager obj) {
+		EntityManager em = Context.getInstance().getEntityManagerFactory().createEntityManager();
+		EntityTransaction tx = null;
+		Passager a = null;
+		try {
+			tx = em.getTransaction();
+			tx.begin();
+			a = em.merge(obj);
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+		} finally {
+			if (em != null && em.isOpen()) {
+				em.close();
+			}
+		}
+		return a;
+	}
+
+	@Override
+	public void delete(Passager obj) {
+		EntityManager em = Context.getInstance().getEntityManagerFactory().createEntityManager();
+		EntityTransaction tx = null;
+		try {
+			tx = em.getTransaction();
+			tx.begin();
+			em.remove(em.merge(obj));
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+		} finally {
+			if (em != null && em.isOpen()) {
+				em.close();
+			}
+		}
+
+	}
+
+	@Override
+	public void deleteByKey(Long key) {
+		EntityManager em = Context.getInstance().getEntityManagerFactory().createEntityManager();
+		EntityTransaction tx = null;
+		try {
+			tx = em.getTransaction();
+			tx.begin();
+			em.remove(em.find(Passager.class, key));
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+		} finally {
+			if (em != null && em.isOpen()) {
+				em.close();
+			}
+		}
+
+	}
+
+	@Override
+	public List<Passager> findAll() {
+		EntityManager em = Context.getInstance().getEntityManagerFactory().createEntityManager();
+		Query query = em.createQuery("from Passager p");
+		List<Passager> passagers = null;
+		passagers = query.getResultList();
+		em.close();
+		return passagers;
+	}
+
+}
