9a5f783a76436b7c2753b1bac07dc2e8a276cccd
Mathieu <mathieu@MacBook-Air-de-Mathieu.local>
2018-10-26 14:44:58 +0200

    corrections DaoGeneric

--- src/main/java/ProjetAeroport/dao/DaoGeneric.java
+++ src/main/java/ProjetAeroport/dao/DaoGeneric.java
@@ -2,24 +2,14 @@ package ProjetAeroport.dao;
 
 import java.util.List;
 
-<<<<<<< HEAD
 public interface DaoGeneric<T, K> {
+	
 	void create(T obj);
 
 	T findByKey(K key);
 
 	T update(T obj);
-
-=======
-public interface DaoGeneric<T,K> {
-
-	void create(T obj);		
 	
-	T findByKey(K key);		
-	
-	T update(T obj);
-	
->>>>>>> b92c361a365e36d1dd6eab157519b989347db0d2
 	void delete(T obj);
 	
 	void deleteByKey(K key);
